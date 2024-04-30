8d2c0de22cf64e9a4aa90e79a3eda4eb5f2ea0ea
Tati Shevchenko <tatilunarsnail@gmail.com>
2024-04-30 13:13:58 +0300

    Task 2 add conditional operator

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,7 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     let totalPrice = quantity * pricePerDroid;
 
-    if (totalPrice > customerCredits) {
-        return "Insufficient funds!";
-    } else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
+    return totalPrice > customerCredits ? "Insufficient funds!" : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
