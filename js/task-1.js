e54f6d2d1f082594e33e386926d769efea45108f
Tati Shevchenko <tatilunarsnail@gmail.com>
2024-04-30 13:17:56 +0300

    Del 1 line task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,5 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     let totalPrice = quantity * pricePerDroid;
-
     return totalPrice > customerCredits ? "Insufficient funds!" : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
